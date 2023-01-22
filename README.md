947590f98b636d96cbf994b39302c3b56e074ae3
Nathan Robinson <nrobinson2000@me.com>
2023-01-22 18:19:01 -0500

    Cleanup

--- README.md
+++ README.md
@@ -1,20 +1,38 @@
 # arch-pi-builder
 
-Create bootable [Arch Linux ARM](https://archlinuxarm.org/) installations for Raspberry Pi (64-bit)
+Create bootable Arch Linux ARM installations for Raspberry Pi (64-bit)
 
-Adapted from:
-https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-4
+# Install dependencies 
 
+sudo pacman -S arch-install-scripts qemu-user-static-binfmt
+sudo pacman -U archlinuxarm-keyring-20140119-2-any.pkg.tar.xz
 
+# Create partitions and filesystems
 
-Install QEMU
+Use fdisk
+Partition 1: vfat (+200M)
+Partition 2: ext4 (rest of disk)
 
-```bash
-yay -S qemu-user-static-binfmt
-```
+sudo mkfs.vfat /dev/sdX1
+sudo mkfs.ext4 /dev/sdX2
 
-Start `systemd-binfmt.service`
+# Mount filesystems
 
-```bash
-sudo systemctl start systemd-binfmt.service
-```
+sudo mount /dev/sdX2 /mnt
+sudo mkdir /mnt/boot
+sudo mount /dev/sdX1 /mnt/boot
+
+# Install system
+
+./install.sh
+
+# Post install setup
+
+sudo arch-chroot /mnt
+passwd
+useradd -mG wheel USERNAME
+passwd USERNAME
+
+vim /etc/hostname
+vim -p /etc/locale.gen /etc/locale.conf
+locale-gen
---
+++ install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Pacstrap and setup Arch Linux ARM
+
+CONF_DIR="conf"
+SKEL_DIR="skel"
+PKGS_DIR="pkgs"
+
+MOUNT="/mnt"
+
+# ASSUMPTIONS
+# 1. Target disk has been partitioned with fdisk
+# 2. Filesystems have been created
+# 3. Filesystems are mounted at /mnt and /mnt/boot
+
+# Start binfmt QEMU arm service
+sudo systemctl start systemd-binfmt.service
+
+PACSTRAP="sudo pacstrap -MKC $CONF_DIR/pacman.conf $MOUNT"
+CHROOT="sudo arch-chroot $MOUNT"
+
+# Basic setup
+
+$PACSTRAP base archlinuxarm-keyring linux-rpi raspberrypi-bootloader
+
+# Disable initramfs
+        
+STOP_INITRAMFS="raspberrypi-stop-initramfs-4-1-aarch64.pkg.tar.xz"
+STOP_INITRAMFS_PKG="$PKGS_DIR/$STOP_INITRAMFS"
+
+sudo cp -f "$STOP_INITRAMFS_PKG" $MOUNT/root
+$CHROOT pacman -U /root/$STOP_INITRAMFS --noconfirm
+
+# Install essential packages
+
+PKGS="sudo dhcpcd openssh nss-mdns neofetch htop vim"
+$PACSTRAP $PKGS
+
+# Enable services
+
+$CHROOT systemctl enable dhcpcd.service
+$CHROOT systemctl enable sshd.service
+$CHROOT systemctl enable avahi-daemon.service
+$CHROOT systemctl enable systemd-timesyncd.service
+
+
+# Timezone
+$CHROOT ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+
+# sudoers config
+sudo cp -f $CONF_DIR/wheel          $MOUNT/etc/sudoers.d
+
+# MDNS config
+sudo cp -f $CONF_DIR/nsswitch.conf  $MOUNT/etc
+
+# fstab
+sudo cp -f $CONF_DIR/fstab          $MOUNT/etc
+
+# Bootloader config
+sudo cp -f $CONF_DIR/cmdline.txt    $MOUNT/boot
+sudo cp -f $CONF_DIR/config.txt     $MOUNT/boot
+
+# Dotfiles
+sudo cp -f $SKEL_DIR/.vimrc         $MOUNT/etc/skel
+sudo cp -f $SKEL_DIR/.bashrc        $MOUNT/etc/skel
+sudo cp -f $SKEL_DIR/.bash_aliases  $MOUNT/etc/skel
+
+
+
+# MORE
+# Copy ssh public key
+
+# Root passwd
+# Make user
+# Locales
+
+
