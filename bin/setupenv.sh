153cff7fd0e23dacd39ebb33df1eb6edb02e0f0b
Nathan Robinson <nrobinson2000@me.com>
2024-05-24 22:21:43 -0400

    Update archlinuxarm-keyring

--- bin/setupenv.sh
+++ bin/setupenv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Prepare host system for building
-sudo pacman -S arch-install-scripts qemu-user-static-binfmt dosfstools parted
-sudo pacman -U pkgs/archlinuxarm-keyring-20140119-2-any.pkg.tar.xz
+sudo pacman -S base-devel arch-install-scripts qemu-user-static-binfmt dosfstools parted
+
+sudo pacman -U pkgs/archlinuxarm-keyring-20240419-1-any.pkg.tar.xz
